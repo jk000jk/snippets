4080a8b890cfe2764f4135c2be0bb23703b95569
jk000jk <jk000jk@ymail.com>
2018-01-14 11:13:25 -0700

    Update grafica.cpp

--- lab5/grafica.cpp
+++ lab5/grafica.cpp
@@ -67,3 +67,49 @@ int main(int argc, char** argv)
 
     return 0;
 }
+
+
+
+// Puncte
+#include <GL/glut.h> void init()
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0); //glPointSize(40.0);
+	glShadeModel(GL_FLAT);
+}
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);   //initializare desen poligon
+	glVertex2f(0.0, 00.0); //stabilire coordonate triunghi glVertex2f(200.0,200.0);//stabilire coordonate triunghi glVertex2f(00.0,200.0);//stabilire coordonate triunghi
+	glEnd();			   //sfisit desenare
+	//executare functie
+	glFlush();
+	glPointSize(40.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POINTS);
+	glVertex2i(100, 300);
+	glVertex2i(200, 300);
+	glPointSize(40.0);
+	glVertex2i(200, 400);
+	glVertex2i(0, 0);
+	glEnd();
+	glFlush();
+}
+void reshape(int w, int h)						//functia redesenare {
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);   //stabilirea viewportului la dimensiunea ferestrei
+glMatrixMode(GL_PROJECTION);					//specificare matrice modificabila la valoare argumentului de modificare
+glLoadIdentity();								//initializarea sistemului de coordonate
+gluOrtho2D(0.0, (GLdouble)w, 0.0, (GLdouble)h); //stabileste volumul de vedere folosind o proiectie ortografica
+}
+void main(int argc, char **argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400, 400);
+	glutInitWindowPosition(150, 150);
+	glutCreateWindow("puncte");
+	init();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+}
